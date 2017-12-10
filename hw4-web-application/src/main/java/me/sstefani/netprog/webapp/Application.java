49d928d4cae001cb38cb686808dd1d3e21d62c79
Simone Stefani <s.stefani95@gmail.com>
2017-12-10 18:02:48 +0100

    Refactor and cleanup

--- hw4-web-application/src/main/java/me/sstefani/netprog/webapp/Application.java
+++ hw4-web-application/src/main/java/me/sstefani/netprog/webapp/Application.java
@@ -13,6 +13,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
@@ -31,25 +33,32 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-//        String[] currencyCodes = {
-//                "AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HRK",
-//                "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN",
-//                "RON", "RUB", "SEK", "SGD", "THB", "TRY", "USD", "ZAR"};
-//
-//        for (String code : currencyCodes) {
-//            currencyRepository.save(new Currency(code));
-//        }
-//
-//        for (Currency currency : currencyRepository.findAll()) {
-//            OkHttpClient client = new OkHttpClient();
-//            Request request = new Request.Builder().url("https://api.fixer.io/latest?base=" + currency.getCode()).build();
-//            Response response = client.newCall(request).execute();
-//
-//            JSONObject obj = new JSONObject(response.body().string());
-//            JSONObject rates = obj.getJSONObject("rates");
-//            for (String key: rates.keySet()) {
-//                rateRepository.save(new Rate(rates.getDouble(key), currency, currencyRepository.findByCode(key)));
-//            }
-//        }
+        persistCurrencies();
+        persistRates();
+    }
+
+    private void persistCurrencies() {
+        String[] currencyCodes = {
+                "AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HRK",
+                "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN",
+                "RON", "RUB", "SEK", "SGD", "THB", "TRY", "USD", "ZAR"};
+
+        for (String code : currencyCodes) {
+            currencyRepository.save(new Currency(code));
+        }
+    }
+
+    private void persistRates() throws IOException {
+        for (Currency currency : currencyRepository.findAll()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url("https://api.fixer.io/latest?base=" + currency.getCode()).build();
+            Response response = client.newCall(request).execute();
+
+            JSONObject obj = new JSONObject(response.body().string());
+            JSONObject rates = obj.getJSONObject("rates");
+            for (String key: rates.keySet()) {
+                rateRepository.save(new Rate(rates.getDouble(key), currency, currencyRepository.findByCode(key)));
+            }
+        }
     }
 }
