b82a705f88d8a91b1b6fc4e390f77abcede13530
Simone Stefani <s.stefani95@gmail.com>
2017-12-14 20:27:52 +0100

    Add API authentication filter

---
+++ hw5-android-todo/todo-api/src/main/java/me/sstefani/todoapi/security/SecurityConstants.java
@@ -0,0 +1,9 @@
+package me.sstefani.todoapi.security;
+
+public class SecurityConstants {
+    static final String SECRET = "Secret";
+    static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    static final String TOKEN_PREFIX = "Bearer ";
+    static final String HEADER_STRING = "Authorization";
+    public static final String REGISTER_URL = "/register";
+}
